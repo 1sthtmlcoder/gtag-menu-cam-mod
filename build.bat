169889cb8f2b347b6bbda4ff1700ea141d4034ad
Mathperson2938490394820394810hrfhgt83hthifjue8ifhgvhvhvbhhbhgutjrtgthbugv <kennethgamerrules@gmail.com>
2025-06-09 15:29:06 -0700

    Delete build.bat

--- build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Compiling VRMenuMod...
-csc /target:library /out:VRMenuMod.dll /reference:Libraries\BepInEx.dll;Libraries\UnityEngine.dll VRMenuMod.cs
-if %ERRORLEVEL% EQU 0 (
-    echo Compile succeeded.
-) else (
-    echo Compile failed.
-)
-pause
